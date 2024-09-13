1ae6605984912c216ae72d1843ff4b3f9343b0d7
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-09-13 15:13:46 +0200

    refactor: address quality issues in script

--- .github/workflows/scripts/get_tutor_config_value.py
+++ .github/workflows/scripts/get_tutor_config_value.py
@@ -1,14 +1,16 @@
-"""Extract a specific variable value from the config.yml file in the current
+"""Extract a specific value given a key from the config.yml file in the current
 directory and print it to the console so it can be used by GitHub Actions steps
 by using the following syntax:
 
-VALUE=$(python ./scripts/get_tutor_config_value.py VARIABLE_NAME)
+VALUE=$(python ./scripts/get_tutor_config_value.py KEY)
 """
 
 import os
 import sys
 
 import yaml
+
+CONFIG_FILE_PATH = "./config.yml"
 
 
 def check_config_file_exists(file_path):
@@ -34,30 +36,27 @@ def load_yaml_config(file_path):
     Returns:
         dict: The content of the config.yml file.
     """
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         return yaml.safe_load(file)
 
 
-def get_config_value(variable_name, config_file_path):
-    """Extract a specific variable value from the YAML configuration.
+def get_config_value(key, config_file_path):
+    """Extract a specific key value from the YAML configuration.
 
     Args:
-        variable_name (str): The name of the variable to get from the config.yml
-        file.
+        key (str): The name of the key to get from the
+        config.yml file.
         config_file_path (str): The path to the config.yml file.
 
     Returns:
-        str: The value of the extracted variable.
+        str: The value of the key.
     """
     check_config_file_exists(config_file_path)
     config = load_yaml_config(config_file_path)
-    value = config.get(variable_name)
+    value = config.get(key)
 
     if not value:
-        print(
-            f"ERROR: {variable_name} not found in the given config.yml",
-            file=sys.stderr
-        )
+        print(f"ERROR: {key} not found in the given config.yml", file=sys.stderr)
         sys.exit(1)
 
     return value
@@ -66,12 +65,11 @@ def get_config_value(variable_name, config_file_path):
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print(
-            "ERROR: Please provide the variable name to extract from config.yml",
+            "ERROR: Please provide the key to extract from config.yml",
             file=sys.stderr,
         )
         sys.exit(1)
 
-    variable_name = sys.argv[1]
-    config_file_path = "./config.yml"
-    value = get_config_value(variable_name, config_file_path)
-    print(value)
+    config_key = sys.argv[1]
+    config_value = get_config_value(config_key, CONFIG_FILE_PATH)
+    print(config_value)
